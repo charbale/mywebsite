da376c994098644ce23e91aecf4f62789eca35af
charbale <charbeldaher914@gmail.com>
2020-07-23 12:00:24 +0300

    Updates

---
+++ precache-manifest.40b61b7466769e7abe4d25bf5e983b53.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b2c25eb29f78563eb981",
+    "url": "/static/js/main.b2c25eb2.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "b2c25eb29f78563eb981",
+    "url": "/static/css/main.4d0a360a.chunk.css"
+  },
+  {
+    "revision": "9d9402ec4f90649c585c7a9e6eff1be8",
+    "url": "/index.html"
+  }
+];
